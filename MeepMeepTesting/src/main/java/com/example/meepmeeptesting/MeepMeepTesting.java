a5dbe9c6dab9dc3ca0f120b5744222700cca3c3f
cnetz <Cameronjnetz@gmail.com>
2024-12-13 12:02:52 -0700

    more meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,21 +12,23 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 50, Math.PI, Math.PI, 12.25)
+                .setConstraints(35, 35, Math.PI, Math.PI, 12.25)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 58, Math.toRadians(-90)))
                         .splineToConstantHeading(new Vector2d(0, 30), -90) // Place first specimen
-                        .waitSeconds(2)
+                        .waitSeconds(6)
                         .setReversed(true)
                         .splineTo(new Vector2d(-32, 38), -Math.PI / 2)
                         .splineToConstantHeading(new Vector2d(-38, 12), -90) // Move towards opposite aliance to push specimen
                         .splineToConstantHeading(new Vector2d(-46, 58), -90) // Push first sample for hp
                         .splineToConstantHeading(new Vector2d(-48, 12), -90) // Go back
                         .splineToConstantHeading(new Vector2d(-56, 58), -90) // Push second sample for hp
-                        .splineToConstantHeading(new Vector2d(-60, 12), -90) // Go back
-                        .setReversed(true)
-                        .lineToY(54)
+                        //.splineToConstantHeading(new Vector2d(-60, 12), -90) // Go back
+                        //.setReversed(true)
+                        //.lineToY(54)
+
+                        //.splineTo(new Vector2d(0, 30), Math.PI / 2)
                         .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
